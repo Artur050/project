1f1d5d01aa03fb8575365f2e1530a0166a1335d9
ArturPatalaha <artur@gapurov.su>
2024-10-14 17:31:51 +0500

    add Suspense

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,11 +1,14 @@
 import { GoogleButton } from "@/components/GoogleButton";
 import { SignInForm } from "@/components/SigninForm";
+import { Suspense } from "react";
 
 export default async function Signin() {
     return (
         <div className=" flex flex-col items-center gap-4">
             <h1>SignIn</h1>
-            <GoogleButton />
+            <Suspense fallback={<>Loading...</>}>
+                <GoogleButton />
+            </Suspense>
             <SignInForm />
         </div>
     )
